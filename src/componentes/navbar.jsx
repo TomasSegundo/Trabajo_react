d8eb7c51155eeaedbe9475f037b01779d3ae1c20
¨Tomas_Segundo¨ <yom.cegundo@gmail.com>
2023-02-21 23:08:54 -0300

    trabajo react tomas segundo

---
+++ src/componentes/navbar.jsx
@@ -0,0 +1,12 @@
+import "./navbar.css"
+
+const Navbar = () => {
+    return <nav className="container">
+        <p>JIJANTES</p>
+        <p>Informacion</p>
+        <p>Tienda</p>
+        <p>nosotros</p>
+    </nav>
+}
+
+export default Navbar
